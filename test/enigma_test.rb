10ebec9fd46d4495c8c4223c925d8b37400ca9c5
lpile <logan.pile@gmail.com>
2019-04-10 20:24:28 -0600

    create more test for helper methods

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,9 +7,50 @@ class EnigmaTest < Minitest::Test
     @enigma = Enigma.new
   end
 
-  def test_it_exists
+  def test_it_exists_and_attributes
     skip
     assert_instance_of Enigma, @enigma
+    assert_equal [], @enigma.a_shift
+    assert_equal [], @enigma.b_shift
+    assert_equal [], @enigma.c_shift
+    assert_equal [], @enigma.d_shift
+  end
+
+  def test_it_can_set_keys
+    skip
+    expected = ['02', '27', '71', '15']
+
+    assert_equal expected, @enigma.set_keys("02715")
+  end
+
+  def test_it_can_set_offset_keys
+    skip
+    expected = ['1', '0', '2', '5']
+
+    assert_equal expected, @enigma.set_offset_keys("040895")
+  end
+
+  def test_it_can_set_final_shifts
+    skip
+    keys = ['1', '0', '2', '5']
+    offset_keys = ['02', '27', '71', '15']
+    expected = ['3', '27', '73', '20']
+
+    assert_equal expected, @enigma.set_final_shift(keys, offset_keys)
+  end
+
+  def test_it_can_set_letter_shifts
+    skip
+    @enigma.set_shift_keys("hello world")
+    a_shift = ['h', 'o', 'r']
+    b_shift = ['e', '', 'l']
+    c_shift = ['l', 'w', 'd']
+    d_shift = ['l', 'o']
+
+    assert_equal a_shift, @enigma.a_shift
+    assert_equal b_shift, @enigma.b_shift
+    assert_equal c_shift, @enigma.c_shift
+    assert_equal d_shift, @enigma.d_shift
   end
 
   def test_encrypt_method_with_key_and_date
