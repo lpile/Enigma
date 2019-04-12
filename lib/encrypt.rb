3c2c16780f9da846b6e81535e90897edc49880e6
lpile <logan.pile@gmail.com>
2019-04-12 16:23:21 -0600

    update encrypt.rb to print out only message

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -4,6 +4,6 @@ enigma = Enigma.new
 message = File.open(ARGV[0], "r")
 encryption = enigma.encrypt(message.read.chop)
 new_file = File.open(ARGV[1], "w")
-new_file.write(encryption)
+new_file.write(encryption[:encryption])
 
 puts "Created #{ARGV[1]} with the key #{enigma.key} and the date #{enigma.date}"
