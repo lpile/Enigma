0a303bcb350f00496a711b3fb63b86d5ea6bff54
lpile <logan.pile@gmail.com>
2019-04-12 20:00:03 -0600

    update enigma to incorporate decryption class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,6 @@
 require './lib/shift'
 require './lib/encryption'
+require './lib/decryption'
 
 class Enigma
   attr_reader :shift
@@ -15,17 +16,8 @@ class Enigma
   end
 
   def decrypt(input_message, input_key = @shift.key, input_date = @shift.date)
-    @shift.key = input_key; @shift.date = input_date
-    decrypt_message = {}
-    message = set_decrypt_message(input_message, input_key, input_date)
-    decrypt_message = {decryption: message, key: @shift.key, date: @shift.date}
-  end
-
-  def set_decrypt_message(message, key, date)
-    (0...message.length).step(4){|i| message[i] = @shift.alphabet[@shift.set_a_shift(key, date).find_index(message[i])]}
-    (1...message.length).step(4){|i| message[i] = @shift.alphabet[@shift.set_b_shift(key, date).find_index(message[i])]}
-    (2...message.length).step(4){|i| message[i] = @shift.alphabet[@shift.set_c_shift(key, date).find_index(message[i])]}
-    (3...message.length).step(4){|i| message[i] = @shift.alphabet[@shift.set_d_shift(key, date).find_index(message[i])]}
-    message
+    @shift.key = input_key
+    @shift.date = input_date
+    Decryption.new(input_message, @shift).summary
   end
 end
