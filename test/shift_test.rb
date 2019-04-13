ec02f5dbb67a34d620d34312bf489cbd2e4d0cff
lpile <logan.pile@gmail.com>
2019-04-13 07:55:57 -0600

    update shift test for 100% coverage

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -17,10 +17,14 @@ class ShiftTest < Minitest::Test
     assert_equal 5, @shift.key.length
   end
 
-  def test_it_can_produce_alphabet_array
+  def test_it_can_produce_date
+    assert_equal 6, @shift.date.length
+  end
+
+  def test_it_can_produce_alphabet
     expected = ("a".."z").to_a << " "
 
-    assert_equal expected, @shift.alphabet_array
+    assert_equal expected, @shift.alphabet
   end
 
   def test_it_can_produce_last_four
@@ -28,22 +32,42 @@ class ShiftTest < Minitest::Test
   end
 
   def test_it_can_set_a_shifts
-    a = @shift.alphabet_array.rotate(3)
+    a = @shift.alphabet.rotate(3)
     assert_equal a, @shift.set_a_shift("02715", "040895")
   end
 
+  def test_it_can_reverse_a_shifts
+    a = @shift.alphabet.rotate(-3)
+    assert_equal a, @shift.set_a_shift("02715", "040895", true)
+  end
+
   def test_it_can_set_b_shifts
-    b = @shift.alphabet_array.rotate(27)
+    b = @shift.alphabet.rotate(27)
     assert_equal b, @shift.set_b_shift("02715", "040895")
   end
 
+  def test_it_can_reverse_b_shifts
+    b = @shift.alphabet.rotate(-27)
+    assert_equal b, @shift.set_b_shift("02715", "040895", true)
+  end
+
   def test_it_can_set_c_shifts
-    c = @shift.alphabet_array.rotate(73)
+    c = @shift.alphabet.rotate(73)
     assert_equal c, @shift.set_c_shift("02715", "040895")
   end
 
+  def test_it_can_reverse_c_shifts
+    c = @shift.alphabet.rotate(-73)
+    assert_equal c, @shift.set_c_shift("02715", "040895", true)
+  end
+
   def test_it_can_set_d_shifts
-    d = @shift.alphabet_array.rotate(20)
+    d = @shift.alphabet.rotate(20)
     assert_equal d, @shift.set_d_shift("02715", "040895")
+  end
+
+  def test_it_can_reverse_d_shifts
+    d = @shift.alphabet.rotate(-20)
+    assert_equal d, @shift.set_d_shift("02715", "040895", true)
   end
 end
